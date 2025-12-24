368f724877cc14ba49f4d81a1d0eb2c53c6f1fdd
dilayuluerguven <uluerguven245@gmail.com>
2025-12-24 16:42:17 +0300

    Initial project structure created for frontend and API

---
+++ todoapp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoAddComponent } from './pages/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from  '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoAddComponent,
+    TodoUpdateComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, HttpClientModule  ],
+  providers: [{provide:'BASE_API_URL',useValue:environment.baseUrl}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
